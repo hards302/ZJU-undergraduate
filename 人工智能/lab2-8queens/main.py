154fa3c2d687584f082d229da0f299ec36883661
abcat302 <55913028+hards302@users.noreply.github.com>
2022-03-03 10:18:00 +0800

    Add files via upload

---
+++ 人工智能/lab2-8queens/main.py
@@ -0,0 +1,72 @@
+import numpy as np           # 提供维度数组与矩阵运算
+import copy                  # 从copy模块导入深度拷贝方法
+from board import Chessboard
+from itertools import permutations
+
+# 基于棋盘类，设计搜索策略
+class Game:
+    def __init__(self, show = True):
+        """
+        初始化游戏状态.
+        """
+        
+        self.chessBoard = Chessboard(show)
+        self.solves = []
+        self.gameInit()
+        
+    # 重置游戏
+    def gameInit(self, show = True):
+        """
+        重置棋盘.
+        """
+        
+        self.Queen_setRow = [-1] * 8
+        self.chessBoard.boardInit(False)
+        
+    ##############################################################################
+    ####                请在以下区域中作答(可自由添加自定义函数)                 #### 
+    ####              输出：self.solves = 八皇后所有序列解的list                ####
+    ####             如:[[0,6,4,7,1,3,5,2],]代表八皇后的一个解为                ####
+    ####           (0,0),(1,6),(2,4),(3,7),(4,1),(5,3),(6,5),(7,2)            ####
+    ##############################################################################
+    #                                                                            #
+    
+    def run(self, row=0):
+        a = list(range(8))
+        for l in list(permutations(a)):
+            self.gameInit(False)
+            flag = 1
+            for i,item in enumerate(l):
+                if self.chessBoard.setQueen(i,item,False) == False:
+                    flag = 0
+                    break
+            if flag == 1 and self.chessBoard.isWin():
+                self.solves.append(list(l))
+#         self.solves.append([0,6,4,7,1,3,5,2])
+
+    #                                                                            #
+    ##############################################################################
+    #################             完成后请记得提交作业             ################# 
+    ##############################################################################
+    
+    def showResults(self, result):
+        """
+        结果展示.
+        """
+        
+        self.chessBoard.boardInit(False)
+        for i,item in enumerate(result):
+            if item >= 0:
+                self.chessBoard.setQueen(i,item,False)
+        
+        self.chessBoard.printChessboard(False)
+    
+    def get_results(self):
+        """
+        输出结果(请勿修改此函数).
+        return: 八皇后的序列解的list.
+        """
+        
+        self.run()
+        return self.solves
+   
